176dc856a628e6aeb3fcd9f1984d9dbe91d14c30
Aryan Kothari <aryanhkothari@gmail.com>
2022-06-02 23:48:08 +0530

    Calling Server & Arrow Funtion in JS

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ function clickHandler () {
 };
 
 btnTranslate.addEventListener("click", clickHandler);
+
+// Calling Serer from Javascript
+var url = "any server's url"
+
+fetch(url)
+// Converting response to json format
+.then(response => response.json())
+// Take the json format file and console it
+.then(json => console.log(json))
+
+// Uderstanding Arrow =>
+fetch(url)
+.then(function responseHandler(response) {return response.json() })
+.then(function logJson(json) {console.log(json) })
