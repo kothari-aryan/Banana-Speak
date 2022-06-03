227bfaac08c864b8692eccd5833b99bb51add52a
Aryan Kothari <aryanhkothari@gmail.com>
2022-06-03 16:15:54 +0530

    ex10: Error handling
    Actual ex10

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ function getTranslationURL(text) {
     return serverURl + "?" + "text=" + text
 }
 
+// Do this is error occurs in fetching URL
+function errorHandler(error) {
+    console.log("error occured:", error)
+}
+
 function clickHandler () {
     var inputText = txtInput.value
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json)
         .then(json => console.log(json.contents.translated))
+        // If error happens call this function
+        .catch(errorHandler)
 };
+
+// Inside Catch Code
+
+// function catch(callbackfn) {
+    // do something...
+    // more something...
+    // callbackfn(error)
+// };
 
 btnTranslate.addEventListener("click", clickHandler);
