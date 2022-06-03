6d6166da915c7b9c3f49fc73d7cf4cfc86b20b70
Aryan Kothari <aryanhkothari@gmail.com>
2022-06-03 16:33:29 +0530

    ex11: Banana Talk

--- app.js
+++ app.js
@@ -2,33 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURl = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
     return serverURl + "?" + "text=" + text
 }
 
-// Do this is error occurs in fetching URL
 function errorHandler(error) {
     console.log("error occured:", error)
+    alert("Something went wrong try again after some time")
 }
 
 function clickHandler () {
     var inputText = txtInput.value
 
     fetch(getTranslationURL(inputText))
-        .then(response => response.json)
-        .then(json => console.log(json.contents.translated))
+        .then(response => response.json())
+        .then(json => {
+            // showing output in website output box
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            })
         // If error happens call this function
         .catch(errorHandler)
 };
-
-// Inside Catch Code
-
-// function catch(callbackfn) {
-    // do something...
-    // more something...
-    // callbackfn(error)
-// };
 
 btnTranslate.addEventListener("click", clickHandler);
