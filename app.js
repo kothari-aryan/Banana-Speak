fc26390d5f23c36e7805f0143879e0152efd2dd6
Aryan Kothari <aryanhkothari@gmail.com>
2022-06-03 15:49:49 +0530

    ex10: Fire fetch call in app
    Fetching only translated value from server

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text) {
+    return serverURl + "?" + "text=" + text
+}
+
 function clickHandler () {
-    outputDiv.innerText = "asdfasda " + txtInput.value
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json)
+        .then(json => console.log(json.contents.translated))
 };
 
 btnTranslate.addEventListener("click", clickHandler);
-
-// Calling Serer from Javascript
-var url = "any server's url"
-
-fetch(url)
-// Converting response to json format
-.then(response => response.json())
-// Take the json format file and console it
-.then(json => console.log(json))
-
-// Uderstanding Arrow =>
-fetch(url)
-.then(function responseHandler(response) {return response.json() })
-.then(function logJson(json) {console.log(json) })
